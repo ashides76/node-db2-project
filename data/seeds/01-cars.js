9c0fe4755b9fc7f1f5af5ffb10dbc0a2d025ff46
Ashish Desai <desai.ashish76@gmail.com>
2024-05-14 18:32:00 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // STRETCH
+const cars = [
+    {
+        vin: '11111111111111',
+        make: 'honda',
+        model: 'civic',
+        milage: 303000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '21111111111111',
+        make: 'toyota',
+        model: 'prius',
+        milage: 300000,
+        title: 'salvage',
+    },
+    {
+        vin: '31111111111111',
+        make: 'ford',
+        model: 'focus',
+        milage: 1500,
+        title: 'clean',
+    },
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+       return knex('cars').insert(cars) 
+    })
+}
+
+// // using async await method
+// exports.seed = async function(knex) {
+//     await knex('cars').truncate()
+//     await knex('cars').insert(cars) 
+// }
