87c43ed9348465b2e2b55401cd7ea7f9643cc321
Ashish Desai <desai.ashish76@gmail.com>
2024-05-14 20:38:28 -0400

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments()
+    table.string('vin', 17).notNullable().unique()
+    table.string('make').notNullable()
+    table.string('model').notNullable()
+    table.numeric('milage').unsighned().notNullable()
+    table.string('title')
+    table.string('transmission')
+  });
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists('cars')
 };
