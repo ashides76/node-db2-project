57fb530bfcb8f967675a265d29bd6d1083888b42
Ashish Desai <desai.ashish76@gmail.com>
2024-05-15 21:16:44 -0400

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    res.json('getting all cars')
+})
+router.get('/:id', async (req, res) => {
+    res.json(`getting a car with id ${req.params.id}`)
+})
+router.post('/', async (req, res) => {
+    res.json('posting new car')
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express")
-
+const carsRouter = require('./cars/cars-router')
 const server = express()
 
 // DO YOUR MAGIC
 server.use(express.json())
+server.use('/api/cars', carsRouter)
 server.use('*', (req, res, next) => {
     next({
         status: 404,
